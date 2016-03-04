3e6c6bd0d0b1921b173eafd0d297fd761c868639
TKosmacki <tfk7@pitt.edu>
2016-03-03 19:34:42 -0500

    Cron Task Added for Accepted Questions

---
+++ crontasks.py
@@ -0,0 +1,18 @@
+import webapp2
+import models
+from google.appengine.ext import ndb
+
+class validate_questions(webapp2.RequestHandler):
+    #just loops and prints every question from query
+    review = models.get_oldest_questions(1000,False) #searches 1000 oldest invalid questions
+    if review:
+        for question_obj in review:
+            if question_obj.score > 10:
+                question_obj.accepted = True
+                question_obj.put()
+                
+mappings = [
+    ('/tasks/validate_questions', validate_questions),
+]
+
+app = webapp2.WSGIApplication(mappings, debug=True)
