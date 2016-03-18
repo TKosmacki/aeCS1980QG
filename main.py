7d0c85a44f079675584da4e34eb4f091245c204f
TKosmacki <tfk7@pitt.edu>
2016-03-18 16:54:06 -0400

    Quiz Displays Properly
    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!

--- main.py
+++ main.py
@@ -392,7 +392,7 @@ class categoryQuiz(webapp2.RequestHandler):
 
         qList = []
         for q in questions:
-            temp = q.to_dict(exclude=['category','creator','accepted','up_voted','down_voted','create_datetime'])
+            temp = q.to_dict(exclude=['category','creator','accepted','up_voters','down_voters','create_datetime'])
             qList.append(temp)
 
         jList = json.dumps(qList, default=obj_dict)
