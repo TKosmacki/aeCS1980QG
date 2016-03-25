b62a96615ca80c3ff074782f03f928255268192b
TKosmacki <tfk7@pitt.edu>
2016-03-25 00:28:27 -0400

    Bunch of Bug Fixes

--- crontasks.py
+++ crontasks.py
@@ -6,10 +6,13 @@ class validate_questions(webapp2.RequestHandler):
     #just loops and prints every question from query
     review = models.get_oldest_questions(1000,False) #searches 1000 oldest invalid questions
     if review:
-        for question_obj in review:
-            if question_obj.score > 0:
-                question_obj.accepted = True
-                question_obj.put()
+        for question in review:
+            if question.score > 0:
+                question.accepted = True
+                question.put()
+            
+            if question.score < 1:
+                #figure out how to delete
                 
 mappings = [
     ('/tasks/validate_questions', validate_questions),
